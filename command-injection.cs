00f5584debfccb3907285fadbf0d04b707cd84c5
jc-anand <161785894+jc-anand@users.noreply.github.com>
2025-11-25 15:21:36 +0000

    Update command-injection.cs

--- command-injection.cs
+++ command-injection.cs
@@ -1,12 +1,1 @@
-using System;
-using System.Diagnostics;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        // BAD: direct use of untrusted input in a system command
-        string userInput = args[0];
-        Process.Start("cmd.exe", "/c " + userInput);
-    }
-}
+//
