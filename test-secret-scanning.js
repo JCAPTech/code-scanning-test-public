2f6c995b2f71e93a73499b606488f1dda8bcd8bb
jc-anand <161785894+jc-anand@users.noreply.github.com>
2025-11-25 16:02:38 +0000

    Update test-secret-scanning.js

--- test-secret-scanning.js
+++ test-secret-scanning.js
@@ -1,4 +1,7 @@
 const awsAccessKey = "AKIA1234567890EXAMPLE";
 const awsSecretKey = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
 
+const key = "AKIA1234567890EXAMPLE";
+
+
 console.log("Testing secret scanning");
